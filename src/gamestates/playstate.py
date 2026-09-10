52160b7d35fc1e61514ce424060a2e603450f2ff
Fahadk999 <fahadkxyz18@gmail.com>
2026-09-11 01:05:38 +0500

    added score text

--- src/gamestates/playstate.py
+++ src/gamestates/playstate.py
@@ -3,10 +3,13 @@ from src.midline import MidLine
 from src.ball import Ball
 from src.paddle import Paddle
 from src.boundry import Boundry
+from src.utils.text import Text
 
 class PlayState:
     def __init__ (self, swidth, sheight):
         self.swidth, self.sheight = swidth, sheight
+        font = "assets/fonts/customfont.otf"
+
         offsetX = swidth//45
         posY = sheight//2
         self.paddleRight = Paddle(offsetX, posY, "#cf0c0c")
@@ -15,19 +18,34 @@ class PlayState:
         self.ball = Ball(swidth, sheight)
         self.boundry = Boundry(swidth, sheight)
 
-        self.allSprites = pygame.sprite.Group()
+        self.scoreLeft = 0
+        self.scoreRight = 0
+        self.scoreLTxt = Text(f"{self.scoreLeft}", 0, 0, font, 40)
+        self.scoreRTxt = Text(f"{self.scoreRight}", 0, 0, font, 40)
+        self.scoreLTxt.rect.topleft = (7,0)
+        self.scoreRTxt.rect.topright = (swidth-7, 0)
+
+        # self.allSprites = pygame.sprite.Group()
 
     def update (self, events, dt, keys, currState):
         self.paddleRight.move(self.sheight, self.ball)
         self.paddleLeft.move(self.sheight, self.ball)
         self.ball.move()
-
+        
+        self.scoreLTxt.updateText(f"{self.scoreLeft}")
+        self.scoreRTxt.updateText(f"{self.scoreRight}")
+        self.scoreLTxt.rect.topleft = (7,0)
+        self.scoreRTxt.rect.topright = (self.swidth-7, 0)
+        
     def draw (self, screen):
         self.midLine.draw(screen)
         self.boundry.draw(screen)
         self.paddleRight.draw(screen)
         self.paddleLeft.draw(screen)
         self.ball.draw(screen)
+
+        self.scoreLTxt.draw(screen)
+        self.scoreRTxt.draw(screen)
 
     def resetGame (self):
         pass
